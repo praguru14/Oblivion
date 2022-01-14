11d2294e613d54b13fdd71d9cb5faa9781de4aa8
Prafull Gururani <praguru14@gmail.com>
2022-01-14 23:45:59 +0530

    Refactor

--- src/main/java/com/pg/obv/services/ProjServicesImpl.java
+++ src/main/java/com/pg/obv/services/ProjServicesImpl.java
@@ -1,27 +1,19 @@
 package com.pg.obv.services;
 
+
 import com.pg.obv.dao.BookRepository;
 import com.pg.obv.models.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Component
 @Service
 public class ProjServicesImpl implements ProjServices {
     @Autowired
     BookRepository bookRepository;
-
-//    private static List<Book> bookData = new ArrayList<>();
-//
-//    sta
-//    tic {
-//        bookData.add(new Book(123,"PG",450,"Prafull"));
-//        bookData.add(new Book(456,"PrafG",1450,"PrafullG"));
-//    }
 
     @Override
     public List<Book>  getBooks() {
@@ -30,7 +22,6 @@ public class ProjServicesImpl implements ProjServices {
     }
 
     public Book getSingleBookById(int id){
-    //Book ID = bookData.stream().filter(e -> e.getId()==id).findFirst().get();
         Book ID  =  bookRepository.findById(id);
     return ID;
     }
@@ -40,19 +31,11 @@ public class ProjServicesImpl implements ProjServices {
         return book;
     }
     public void DeleteBook(int id){
-    //Book deletedBook = bookData.stream().filter(e ->e.getId()!=id).findFirst().get();
         bookRepository.deleteById(id);
     }
 
     @Override
     public void updateBook(Book book, int id) {
-//       bookData=  bookData.stream().map(b->{
-//                if(b.getId()==id){
-//                    b.setValue(book.getValue());
-//                }
-//                return b;
-//                }).collect(Collectors.toList());
-
         book.setId(id);
         bookRepository.save(book);
     }
