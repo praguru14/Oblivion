8af8a92ffd980151939bca632563116112359157
Prafull Gururani <praguru14@gmail.com>
2022-01-22 00:10:14 +0530

    Added: Dynamic Image upload code

--- src/main/java/com/pg/obv/controllers/FileUploadController.java
+++ src/main/java/com/pg/obv/controllers/FileUploadController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class FileUploadController {
@@ -20,17 +21,13 @@ public class FileUploadController {
     @PostMapping("/upload")
     public ResponseEntity<String> uploadFile(@RequestParam("image")MultipartFile file){
         try{
-
             Boolean check = fileUploadHelper.fileUploadOrNot(file);
-
-            if(check){
-                return new ResponseEntity<>("File Uploaded", HttpStatus.OK);
-            }
-            return new ResponseEntity<>("error file not uploaded",HttpStatus.NO_CONTENT);
+            if(check)
+                return new ResponseEntity<>(ServletUriComponentsBuilder.fromCurrentContextPath().path("images/").path(file.getOriginalFilename()).toUriString(),HttpStatus.OK);
         }
         catch (Exception e){
             e.printStackTrace();
-            return new ResponseEntity<>("error",HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<>("error",HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/pg/obv/fileUpload/FileUploadHelper.java
+++ src/main/java/com/pg/obv/fileUpload/FileUploadHelper.java
@@ -1,24 +1,49 @@
 package com.pg.obv.fileUpload;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 @Component
 public class FileUploadHelper {
-    private final String UPLOAD_DIR= "/home/prafull/Documents/workspace-spring-tool-suite-4-4.13.0.RELEASE/Oblivion/src/main/resources/static/images";
 
-    public boolean fileUploadOrNot(MultipartFile file) throws IOException {
+    private final String UPLOAD_DIR = new ClassPathResource("static/images/").getFile().getAbsolutePath();
+
+    public FileUploadHelper() throws IOException {
+    }
+
+    public boolean fileUploadOrNot(MultipartFile file){
     boolean fal = false;
 
+            try{
+                Files.copy(file.getInputStream(), Paths.get(UPLOAD_DIR+ File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+                fal = true;
+            }
+            catch (Exception e){
+                fal = false;
+            }
+
+    return fal;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------
+//private final String UPLOAD_DIR= "/home/prafull/Documents/workspace-spring-tool-suite-4-4.13.0.RELEASE/Oblivion/src/main/resources/static/images";
 //        InputStream input = file.getInputStream();
 //        byte data[]= new byte[input.available()];
 //        input.read(data);
@@ -28,13 +53,3 @@ public class FileUploadHelper {
 //        fos.write(data);
 //        fos.flush();
 //        fos.close();
-            try{
-                Files.copy(file.getInputStream(), Paths.get(UPLOAD_DIR+ File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
-                fal = true;
-            }
-            catch (Exception e){
-                fal = false;
-            }
-    return fal;
-    }
-}
